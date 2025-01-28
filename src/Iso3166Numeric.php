5250dc64ce2a1e738311d29ec0c7c4410c394c8a
Eugene <jekkogm@gmail.com>
2025-01-28 17:28:00 +0200

    Fixed code style

--- src/Iso3166Numeric.php
+++ src/Iso3166Numeric.php
@@ -9,7 +9,6 @@ use Jekk0\Laravel\Iso3166\Validation\Rules\Classes\Iso3166BaseRule;
 
 class Iso3166Numeric extends Iso3166BaseRule
 {
-
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
         if ($this->validator->isValidNumericCountryCode((int)$value) === false) {
